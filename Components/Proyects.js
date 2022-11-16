ed6b1393bf6a759d6d44ef090766c877af3d2182
BrianGC <gargallocarnallabrianhermam@gmail.com>
2022-11-15 21:04:27 -0600

    correciones

--- Components/Proyects.js
+++ Components/Proyects.js
@@ -29,7 +29,7 @@ export default function Proyects({proyects}) {
           </div>
           <div className="basis-1/3 flex-1">
             <Image src={web4} alt="web4" className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" />
-          </div>
+          </div> 
 
         </div>
       </section>
